8dd143dadbbb20e36d09a7dca26989844f845aa9
Momowito <Momo.crouzet@gmail.com>
2024-05-31 16:15:51 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    // Import des ressources 
+    require_once './service/db_connect.php';
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TP</title>
+</head>
+<body>
+  
+
+</body>
+</html>
